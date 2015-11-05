4fbf86bbb9991feadac253e60b36b6ed1469ba1f
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-11-05 13:08:17 +0400

    Issue 4430: Specify connection params

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
 override['redisio']['version'] = '3.0.5'
+override['redisio']['servers'] = [
+    { name: '', address: '127.0.0.1', port: 6379 }
+]
 
 node.from_file(run_context.resolve_attribute('redisio', 'default'))
