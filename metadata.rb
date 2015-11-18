6197b55b54aa3179016bbfc4e22e092ab4d3f216
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-11-18 10:47:27 +0400

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,9 @@
 name 'latest-redis'
+maintainer 'Alexander Pyatkin'
+maintainer_email 'aspyatkin@gmail.com'
+license 'MIT'
 description 'Installs and configures redis'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
 recipe 'latest-redis', 'Installs and configures redis'
