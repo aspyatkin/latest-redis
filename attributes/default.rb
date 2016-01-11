cccbe27710170a6000873c4a3438d5a63f38e35a
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-01-11 21:55:28 +0400

    Small correction

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,3 @@ id = 'latest-redis'
 default[id][:version] = '3.0.6'
 default[id][:listen][:address] = '127.0.0.1'
 default[id][:listen][:port] = 6379
-
-if node['platform'] == 'ubuntu' && Gem::Version.new(node['platform_version']) >= Gem::Version.new('15.10')
-  default[:redisio][:job_control] = 'systemd'
-end
