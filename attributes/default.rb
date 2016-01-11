b961a3ba9d4d6d3918c9fcef52e5166af028a434
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-01-11 21:54:13 +0400

    Set wrapped cookook attributes inside recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,9 @@
-override['redisio']['version'] = '3.0.5'
-override['redisio']['servers'] = [
-    { name: '', address: '127.0.0.1', port: 6379 }
-]
+id = 'latest-redis'
 
-node.from_file(run_context.resolve_attribute('redisio', 'default'))
+default[id][:version] = '3.0.6'
+default[id][:listen][:address] = '127.0.0.1'
+default[id][:listen][:port] = 6379
+
+if node['platform'] == 'ubuntu' && Gem::Version.new(node['platform_version']) >= Gem::Version.new('15.10')
+  default[:redisio][:job_control] = 'systemd'
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,13 @@
+id = 'latest-redis'
+
+node.default[:redisio][:version] = node[id][:version]
+node.default[:redisio][:servers] = [
+  {
+    name: nil,
+    address: node[id][:listen][:address],
+    port: node[id][:listen][:port]
+  }
+]
+
 include_recipe 'redisio'
 include_recipe 'redisio::enable'
