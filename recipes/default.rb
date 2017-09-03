b9842488a7db78c217ed03a5c3c8c8da217ed37b
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-09-03 14:48:08 +0400

    Update Redis major version

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,11 @@
 id = 'latest-redis'
 
-node.default[:redisio][:version] = node[id][:version]
-node.default[:redisio][:servers] = [
+node.default['redisio']['version'] = node[id]['version']
+node.default['redisio']['servers'] = [
   {
     name: nil,
-    address: node[id][:listen][:address],
-    port: node[id][:listen][:port]
+    address: node[id]['listen']['address'],
+    port: node[id]['listen']['port']
   }
 ]
 
