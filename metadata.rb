bdfb3142d686d10a3bc405c893c5671d5fd9e49c
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-06-01 14:18:31 +0400

    Setup password

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
 description 'Installs and configures redis'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.3.0'
+version '1.3.1'
 
 recipe 'latest-redis', 'Installs and configures redis'
 depends 'redisio', '~> 2.7.1'
+depends 'secret', '~> 1.0.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,13 @@
 id = 'latest-redis'
+secret = ::ChefCookbook::Secret::Helper.new(node)
 
 node.default['redisio']['version'] = node[id]['version']
 node.default['redisio']['servers'] = [
   {
     name: nil,
     address: node[id]['listen']['address'],
-    port: node[id]['listen']['port']
+    port: node[id]['listen']['port'],
+    requirepass: secret.get('redis:password', required: false, default: nil)
   }
 ]
 
